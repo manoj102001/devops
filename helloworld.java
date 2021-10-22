1f9ca362bb9dbbe87efd8b8b41e54629c9ceed46
manoj102001 <92981970+manoj102001@users.noreply.github.com>
2021-10-23 00:05:46 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
-public class myfirstclass{
-public static void main(String[] args)
-{
-            System.out.println(Hello World);
-}
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
